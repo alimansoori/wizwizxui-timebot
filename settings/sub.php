212f88062495ece7a85beab1d967e475d43868b6
Ali Mansoori <alimanssouri221@gmail.com>
2025-08-29 03:21:16 +0330

    u

--- settings/sub.php
+++ settings/sub.php
@@ -243,7 +243,7 @@ if (!empty($links)) {
     $randomId = uuidv4_random();
     $daysHeader = $minDaysLeft !== null ? $minDaysLeft : 0;
 
-    $headerRemarkText = '📊 مصرف شما: ' . $usage . ' گیگابایت از ' . $volume . ' گیگابایت 📊';
+    $headerRemarkText = '📊 مصرف شما: ' . $usage . ' گیگابایت از ' . $catId . ' گیگابایت 📊';
     $usageLink = 'vless://' . $randomId . '@127.0.0.1:1?type=none&encryption=none#'
         . rawurlencode($headerRemarkText);
 
