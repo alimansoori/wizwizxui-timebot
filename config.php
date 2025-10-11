f520e2c64b6e8fc742b22d4c7245284c7efe5e5d
Ali Mansoori <alimanssouri221@gmail.com>
2025-10-11 12:23:08 +0330

    Update

--- config.php
+++ config.php
@@ -6412,7 +6412,7 @@ function getJson($server_id)
     // sendMessage(json_encode(['cookieHeader' => $cookieHeader]), null, null, $admin);
 
     if ($serverType == "sanaei")
-        $url = "$panel_url/panel/inbound/list";
+        $url = "$panel_url/panel/api/inbounds/list";
     else
         $url = "$panel_url/xui/inbound/list";
 
@@ -6425,7 +6425,7 @@ function getJson($server_id)
         CURLOPT_TIMEOUT => 15,
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_CUSTOMREQUEST => 'GET',
         CURLOPT_HEADER => false,
         CURLOPT_HTTPHEADER => array(
             'User-Agent:  Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
@@ -6440,11 +6440,8 @@ function getJson($server_id)
     ));
 
     $response = curl_exec($curl);
-    $errNo = curl_errno($curl);
-    $errMsg = curl_error($curl);
-    $info = curl_getinfo($curl);
 
-    // sendMessage(json_encode(['response' => $response]), null, null, $admin);
+    sendMessage(json_encode(['response' => $response]), null, null, $admin);
 
     curl_close($curl);
     return json_decode($response);
